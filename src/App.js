a3fdfe85c8689a50821744bb304dd0d1e59daa2f
Ursula Cubilla <ursulacubilla@gmail.com>
2023-06-02 16:59:55 +0200

    primer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Application } from './Components/Application';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Application />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
